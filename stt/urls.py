735d8a4ffb3ff1fe361b542fc6e32ff67be94f54
abirukov <abirukov2008@gmail.com>
2024-03-03 12:55:45 +0300

    add favicon

--- stt/urls.py
+++ stt/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -23,6 +24,12 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        path(
+            "favicon.ico",
+            RedirectView.as_view(url=settings.STATIC_URL + "img/favicon.ico"),
+        )
+    ]
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
