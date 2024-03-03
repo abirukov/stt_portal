2e2180e5be3ed3cd4e2ffc0ca803a421f0e1f49b
abirukov <abirukov2008@gmail.com>
2024-03-03 18:40:47 +0300

    Homepage

--- stt/home/models.py
+++ stt/home/models.py
@@ -1,4 +1,9 @@
+from django.db import models
+from wagtail.admin.panels import MultiFieldPanel, FieldPanel
+from wagtail.fields import StreamField, RichTextField
 from wagtail.models import Page
+
+from stt.base.blocks import BaseStreamBlock
 
 
 class HomePage(Page):
@@ -8,3 +13,92 @@ class HomePage(Page):
         "news.NewsSectionPage",
         "knowledge.KnowledgeSectionPage",
     ]
+
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Homepage image",
+    )
+    hero_text = models.CharField(
+        max_length=255, help_text="Write an introduction for the bakery", null=True,
+    )
+
+    # Body section of the HomePage
+    body = StreamField(
+        BaseStreamBlock(),
+        verbose_name="Home content block",
+        blank=True,
+        use_json_field=True,
+    )
+
+    # Promo section of the HomePage
+    promo_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Promo image",
+    )
+    promo_title = models.CharField(
+        blank=True, max_length=255, help_text="Title to display above the promo copy"
+    )
+    promo_text = RichTextField(
+        null=True, blank=True, max_length=1000, help_text="Write some promotional copy"
+    )
+
+    # Featured sections on the HomePage
+    # You will see on templates/base/home_page.html that these are treated
+    # in different ways, and displayed in different areas of the page.
+    # Each list their children items that we access via the children function
+    # that we define on the individual Page models e.g. BlogIndexPage
+    featured_section_1_title = models.CharField(
+        blank=True, max_length=255, help_text="Title to display above the promo copy"
+    )
+    featured_section_1 = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="First featured section for the homepage. Will display up to "
+                  "three child items.",
+        verbose_name="Featured section 1",
+    )
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("image"),
+                FieldPanel("hero_text"),
+            ],
+            heading="Hero section",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("promo_image"),
+                FieldPanel("promo_title"),
+                FieldPanel("promo_text"),
+            ],
+            heading="Promo section",
+        ),
+        FieldPanel("body"),
+        MultiFieldPanel(
+            [
+                MultiFieldPanel(
+                    [
+                        FieldPanel("featured_section_1_title"),
+                        FieldPanel("featured_section_1"),
+                    ]
+                ),
+            ],
+            heading="Featured homepage sections",
+        ),
+    ]
+
+
+    def __str__(self):
+        return self.title
