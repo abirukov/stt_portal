b0d238bb65dba411a58b2d5e28fc356de79ffed2
abirukov <abirukov2008@gmail.com>
2024-03-04 11:14:11 +0300

    auth

--- stt/settings/base.py
+++ stt/settings/base.py
@@ -17,7 +17,7 @@ from stt.config import config
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
-
+ALLOWED_HOSTS: list[str | None] = config.allowed_hosts
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
