d2f2c23eadfc00ed485581b424a77c61f72b45af
abirukov <abirukov2008@gmail.com>
2024-03-04 11:14:31 +0300

    auth

--- stt/settings/dev.py
+++ stt/settings/dev.py
@@ -3,12 +3,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!4jlgyqvu*tfq2=khny(cb-(3)36-bngjr4uqjgt5irjf5a*-y"
-
-# SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
-
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
