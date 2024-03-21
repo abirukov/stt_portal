e4a78fdba7a10d635307c5ccbf7f077b8bb7946b
abirukov <abirukov2008@gmail.com>
2024-03-21 14:17:45 +0300

    new design - clear

--- stt/news/templatetags/news_tags.py
+++ stt/news/templatetags/news_tags.py
@@ -8,9 +8,6 @@ from stt.news.models import NewsPage
 register = template.Library()
 
 
-# Retrieves a single gallery item and returns a gallery of images
-
-
 @register.inclusion_tag("tags/news_section.html", takes_context=True)
 def news_section(context: RequestContext) -> dict[str, Any]:
     news = NewsPage.objects.all()
