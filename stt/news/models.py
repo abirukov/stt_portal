5ddfd19dce0d20145ae11999f4e040708ca34c17
abirukov <abirukov2008@gmail.com>
2024-03-22 09:38:48 +0300

    new design - новости

--- stt/news/models.py
+++ stt/news/models.py
@@ -1,4 +1,6 @@
-from stt.base.models import SectionPage, StandardPage
+from django.db.models import QuerySet
+
+from stt.base.models import SectionPage, StandardPage, PaginatedPage
 
 
 class NewsPage(StandardPage):
@@ -7,8 +9,14 @@ class NewsPage(StandardPage):
         verbose_name_plural = "Страницы новостей"
 
 
-class NewsSectionPage(SectionPage):
+class NewsSectionPage(PaginatedPage, SectionPage):
     subpage_types: list[str] = ["news.NewsPage"]
+
+    content_panels = SectionPage.content_panels + PaginatedPage.content_panels
+
+    @property
+    def elements(self) -> QuerySet:
+        return NewsPage.objects.all()
 
     class Meta:
         verbose_name = "Раздела новостей"
