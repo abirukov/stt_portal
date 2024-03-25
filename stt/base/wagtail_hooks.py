45b46d8c55398a232b19cdd615c5b830c70c9753
abirukov <abirukov2008@gmail.com>
2024-03-25 09:29:29 +0300

    new design - wagtail_userbar add

---
+++ stt/base/wagtail_hooks.py
@@ -0,0 +1,14 @@
+from wagtail import hooks
+from wagtail.admin.userbar import AccessibilityItem
+
+
+class CustomAccessibilityItem(AccessibilityItem):
+    axe_run_only = None
+
+
+@hooks.register("construct_wagtail_userbar")
+def replace_userbar_accessibility_item(request, items):
+    items[:] = [
+        CustomAccessibilityItem() if isinstance(item, AccessibilityItem) else item
+        for item in items
+    ]
